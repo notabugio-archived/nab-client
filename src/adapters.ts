90428b5d391d9162c399271f36251451f5af2634
go1dfish <me@go1dfish.me>
2019-12-25 00:01:50 -0800

    Support federation opts

--- src/adapters.ts
+++ src/adapters.ts
@@ -1,11 +1,15 @@
-import { FederationAdapter } from '@chaingun/federation-adapter'
+import {
+  FederatedAdapterOpts,
+  FederationAdapter
+} from '@chaingun/federation-adapter'
 import { createGraphAdapter as createHttpAdapter } from '@chaingun/http-adapter'
 import { GunGraphAdapter } from '@chaingun/sea-client'
 
 export function createFederationAdapter(
   internal: GunGraphAdapter,
   peerUrls: readonly string[],
-  persist?: GunGraphAdapter
+  persist?: GunGraphAdapter,
+  opts?: FederatedAdapterOpts
 ): GunGraphAdapter {
   const peers: Record<string, GunGraphAdapter> = peerUrls.reduce((pm, url) => {
     return {
@@ -13,7 +17,7 @@ export function createFederationAdapter(
       [url]: createHttpAdapter(`${url}/gun`)
     }
   }, {})
-  const adapter = FederationAdapter.create(internal, peers, persist)
+  const adapter = FederationAdapter.create(internal, peers, persist, opts)
 
   return adapter
 }
